f71961ee44a4cd0d8a3b1042cd6ef09a66b2a23e
Dariusz Okoński <darek200180@gmail.com>
2024-04-17 07:19:48 +0200

    add fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,36 @@
+import data from './data.json';
+
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  // current item
+
+  const fetchJobs = async () => {
+    const response = data;
+    setJobs(response);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    try {
+      fetchJobs();
+    } catch (err) {}
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className='jobs-center'>
+        <div className='loading'></div>
+      </section>
+    );
+  }
+
   return <h2>Tabs Starter</h2>;
 };
 export default App;
